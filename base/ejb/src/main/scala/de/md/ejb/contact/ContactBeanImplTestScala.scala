f99e14bb4bc39343f85ec9a91d8ad892416b6a14
Martin Dilger <martin.dilger@googlemail.com>
2010-10-16 20:15:21 +0200

    Scala Maven Repo added, First no op Scala Unit Test

---
+++ base/ejb/src/main/scala/de/md/ejb/contact/ContactBeanImplTestScala.scala
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package de.md.ejb.contact
+
+/**
+ * @author dilgerma
+ *
+ */
+object ContactBeanImplTestScala {
+    override def setUp() = {
+	println("setUp called");
+    }
+    
+    override def tearDown() = {
+	println("tearDown called");
+    }
+}
